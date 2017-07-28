d12a20cf0c523c70471aeb590d653d9de25dae56
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2017-07-27 21:32:32 -0300

    Curso de Python 3 - Alura

---
+++ jogos/jogos.py
@@ -0,0 +1,23 @@
+import forca
+import adivinhacao
+
+def selecionar_jogos():
+    print("**********************************************")
+    print("*** Bem vindo  | JOGOS | Selecione e jogue ***")
+    print("**********************************************")
+
+    print("Selecione: ")
+    print("[1] = Jogo da forca")
+    print("[2] = Jogo de adivinhação")
+
+    jogo = int(input("Qual jogo você escolhe? "))
+
+    if (jogo == 1):
+        print("forca")
+        forca.jogar()
+    else:
+        print("adivinhação")
+        adivinhacao.jogar()
+
+if (__name__ == "__main__"):
+    selecionar_jogos()
