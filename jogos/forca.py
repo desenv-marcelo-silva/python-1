e31e3d8cfa4f65ed25e2afc16151fb30a231eccb
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2017-07-27 21:55:33 -0300

    Implementação do chute e da busca por letras na string.

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,5 +3,21 @@ def jogar():
     print("*** Bem vindo ao jogo da forca ***")
     print("**********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        chute = input("Qual a letra? ")
+
+        posicao = 0
+        for letra in palavra_secreta:
+            posicao = posicao + 1
+            if (letra == chute):
+                print("A letra {} foi encontrada na posição {}".format(chute, posicao))
+
+    print("Fim de jogo.")
+
 if (__name__ == "__main__"):
     jogar()
