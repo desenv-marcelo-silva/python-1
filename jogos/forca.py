f0c30e19814d83ed5a6a0f742381c4a24932ea18
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2017-08-09 21:24:50 -0300

    Inserindo lista e testar letras acertadas.

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,9 +1,23 @@
+import random
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "maça".upper()
+
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
     letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
@@ -37,5 +51,5 @@ def jogar():
         print("Você perdeu!!")
     print("Fim do jogo")
 
-if (__name__ == "__main__"):
+if(__name__ == "__main__"):
     jogar()
